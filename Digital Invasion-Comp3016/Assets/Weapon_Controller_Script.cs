5097f0e9bfc1ab7208bf3d87de8413b6da1ad195
Guardsman111 <charliep763@googlemail.com>
2020-11-08 18:00:27 +0000

    Weapon Animation Script
    Created script to update weapon position to hand

---
+++ Digital Invasion-Comp3016/Assets/Weapon_Controller_Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Controller_Script : MonoBehaviour
+{
+    [SerializeField]
+    GameObject HandBone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = HandBone.transform.position;
+        transform.rotation = HandBone.transform.rotation;
+    }
+}
