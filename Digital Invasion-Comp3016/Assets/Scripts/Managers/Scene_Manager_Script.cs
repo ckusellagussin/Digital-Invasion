dbec45ab1063525356ad4558e4a2cbcb00a1c1c5
Morgan Hill <morgan.hill@students.plymouth.ac.uk>
2021-01-13 19:02:49 +0000

    Updated UI
    Added a menu between the main menu and the game it has 4 toggles,4 dropdowns stating different classes with details and stats details and starts change on changed dropdown value. does not yet pass information to game

--- Digital Invasion-Comp3016/Assets/Scripts/Managers/Scene_Manager_Script.cs
+++ Digital Invasion-Comp3016/Assets/Scripts/Managers/Scene_Manager_Script.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Manager_Script : MonoBehaviour
 {
+    public GameObject Page1;
+    public GameObject Page2;
+
+
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -18,5 +22,18 @@ public class Scene_Manager_Script : MonoBehaviour
     public void BackToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+    public void switch_Pages(int PageNo)
+    {
+        if (PageNo == 0)
+        {
+            Page1.SetActive(true);
+            Page2.SetActive(false);
+        }
+        if (PageNo == 1)
+        {
+            Page1.SetActive(false);
+            Page2.SetActive(true);
+        }
     }
 }
---
+++ Digital Invasion-Comp3016/Assets/Scripts/Managers/Unit_Selection_Script.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Unit_Selection_Script : MonoBehaviour
+{
+    List<string> Classes = new List<string>() { "A well sounded soldier, lightly armored balancing durablity and manuverablity fireing at enemies from a fair distance \n \n \nStats: \nMovement:10 \nArmour:5 \nHealth:20 \ndamage:10 \nVision:10 \nFiring Range:10" , "Higher movement and vision range than other units but fragile good for finding the enemy or flanking to put on the pressure!\n \n \nStats: \nMovement:15 \nArmour:0 \nHealth:20 \ndamage:10 \nVision:15 \nFiring Range:15", "Comes packing a shotgun, with high damage but a low firing range get in close using cover and deal the damage you need to  \n \n \nStats: \nMovement:10 \nArmour:5 \nHealth:20 \ndamage:15 \nVision:10 \nFiring Range:5", "Heavily armed and armored the support doesn't move far or fast but can deal and take a lot of damage \n \n \n \nStats: \nMovement:8 \nArmour:7 \nHealth:20 \ndamage:15 \nVision:10 \nFiring Range:10" };
+
+    public Dropdown Unit1;
+    public Dropdown Unit2;
+    public Dropdown Unit3;
+    public Dropdown Unit4;
+
+    public Text Unit1Text;
+    public Text Unit2Text;
+    public Text Unit3Text;
+    public Text Unit4Text;
+
+
+
+
+    public void start()
+    {
+        FillList();
+    }
+
+   public void FillList()
+    {
+        Unit1.AddOptions(Classes);
+        Unit2.AddOptions(Classes);
+        Unit3.AddOptions(Classes);
+        Unit4.AddOptions(Classes);
+
+    }
+    public void DropdownChanged1(Dropdown Unit1)
+    {
+        Unit1Text.text = Classes[Unit1.value];
+        
+
+    }
+    public void DropdownChanged2(Dropdown Unit2)
+    {
+        Unit2Text.text = Classes[Unit2.value];
+
+
+    }
+    public void DropdownChanged3(Dropdown Unit3)
+    {
+        Unit3Text.text = Classes[Unit3.value];
+
+
+    }
+    public void DropdownChanged4(Dropdown Unit4)
+    {
+        Unit4Text.text = Classes[Unit4.value];
+
+
+    }
+}
